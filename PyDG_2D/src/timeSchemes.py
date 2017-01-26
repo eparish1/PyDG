fc32f9ce85729e285538758480092a76e2b98823
Eric <eric.parish9@gmail.com>
2017-01-26 12:41:41 -0500

    update

--- PyDG_2D/src/timeSchemes.py
+++ PyDG_2D/src/timeSchemes.py
@@ -1,11 +1,11 @@
 import numpy as np
 from turb_models import tauModel
-def advanceSol(main,eqns,schemes):
+def advanceSol(main,MZ,eqns,schemes):
   main.a0[:] = main.a.a[:]
   rk4const = np.array([1./4,1./3,1./2,1.])
   for i in range(0,4):
     main.getRHS(main,eqns,schemes)  ## put RHS in a array since we don't need it
-    #w = tauModel(main,MZ,eqns,schemes)
-    main.a.a[:] = main.a0 + main.dt*rk4const[i]*(main.RHS[:])
+    w = tauModel(main,MZ,eqns,schemes)
+    main.a.a[:] = main.a0 + main.dt*rk4const[i]*(main.RHS[:]+w)
   main.t += main.dt
   main.iteration += 1
