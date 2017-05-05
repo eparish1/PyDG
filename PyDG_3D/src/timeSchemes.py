e0c29a1ac755fbda98d003ec0c5ed4fcd1eb34eb
Eric Parish <eric.parish9@gmail.com>
2017-05-05 15:38:54 -0400

    fast implicit

--- PyDG_3D/src/timeSchemes.py
+++ PyDG_3D/src/timeSchemes.py
@@ -1,8 +1,10 @@
 import numpy as np
 from turb_models import tauModel
+from init_Classes import variables
 from scipy.sparse.linalg import LinearOperator
-from scipy.sparse.linalg import gmres
+from scipy.sparse.linalg import gmres,bicgstab
 from scipy.sparse.linalg import lgmres
+from myGMRES import GMRes
 def advanceSol(main,MZ,eqns,schemes):
   main.a0[:] = main.a.a[:]
   rk4const = np.array([1./4,1./3,1./2,1.])
@@ -21,12 +23,111 @@ def advanceSol(main,MZ,eqns,schemes):
 
 
 def advanceSolImplicitMG(main,MZ,eqns,schemes):
+  main_coarse = variables(main.Nel,main.order/2,main.quadpoints,eqns,main.mu,main.xG,main.yG,main.zG,main.t,main.et,main.dt,main.iteration,main.save_freq,schemes,'DNS',main.procx,main.procy)
   old = np.zeros(np.shape(main.a0))
   old[:] = main.a0[:]
   main.a0[:] = main.a.a[:]
+  main_coarse.a.a[:] = main.a.a[:,0:main.order/2,0:main.order/2,0:main.order/2]
+  main_coarse.a0[:] = main.a.a[:,0:main.order/2,0:main.order/2,0:main.order/2]
   main.getRHS(main,eqns,schemes)
+  main_coarse.getRHS(main_coarse,eqns,schemes)
   R0 = np.zeros(np.shape(main.RHS))
   R0[:] = main.RHS[:]
+  R0_coarse = np.zeros(np.shape(main_coarse.RHS))
+  R0_coarse[:] = main_coarse.RHS[:]
+  print(np.linalg.norm(main_coarse.a.u))
+  def mv(v):
+    vr = np.reshape(v,np.shape(main.a.a))
+    eps = 1.e-5
+    main.a.a[:] = main.a0[:] + eps*vr
+    main.getRHS(main,eqns,schemes)
+    R1 = np.zeros(np.shape(main.RHS))
+    R1[:] = main.RHS[:]
+    Av = vr - main.dt/2.*(R1 - R0)/eps
+    main.a.a[:] = main.a0[:] 
+    return Av.flatten()
+
+  def mv_resid(v,b):
+    return mv(v) - b
+
+  def mv_coarse(v):
+    vr = np.reshape(v,np.shape(main_coarse.a.a))
+    eps = 1.e-5
+    main_coarse.a.a[:] = main_coarse.a0[:] + eps*vr
+    main_coarse.getRHS(main_coarse,eqns,schemes)
+    R1 = np.zeros(np.shape(main_coarse.RHS))
+    R1[:] = main_coarse.RHS[:]
+    Av = vr - main_coarse.dt/2.*(R1 - R0_coarse)/eps
+    main_coarse.a.a[:] = main_coarse.a0[:] 
+    return Av.flatten()
+
+
+  def printnorm(r):
+    print('GMRES norm = ' + str(np.linalg.norm(r)))
+
+  A = LinearOperator( (np.size(main.a.a),np.size(main.a.a)), matvec=mv )
+  A_coarse = LinearOperator( (np.size(main_coarse.a.a),np.size(main_coarse.a.a)), matvec=mv_coarse )
+  ## preconditioner type thing
+  print('Running Coarse Mesh')
+  print(np.linalg.norm(R0[:,0:main.order/2,0:main.order/2,0:main.order/2] - R0_coarse) )
+  e = gmres(A_coarse,main.dt*R0_coarse.flatten(), np.zeros(np.shape(R0_coarse.flatten())), tol=1e-05, restart=60, maxiter=60, xtype=None, M=None, callback=printnorm,restrt=None)
+  x0 = np.zeros(np.shape(main.a.a))
+  x0[:,0:main.order/2,0:main.order/2,0:main.order/2] += np.reshape( e[0] , np.shape(main_coarse.a.a) )
+  sol = gmres(A, main.dt*R0.flatten(), x0=x0.flatten(), tol=1e-04, restart=60, maxiter=None, xtype=None, M=None, callback=printnorm,restrt=None)
+  ## multigrid
+#  sol = gmres(A, main.dt*R0.flatten(), x0=old.flatten(), tol=2e-02, restart=60, maxiter=10, xtype=None, M=None, callback=printnorm,restrt=None)[0]
+#  #sol = GMRes(A, main.dt*R0.flatten(), old.flatten(),5)
+#
+#  R =  np.reshape( mv_resid(sol,main.dt*R0.flatten()) , np.shape(main.a.a))
+#  R_coarse = R[:,0:main.order/2,0:main.order/2,0:main.order/2]
+#  print('Running Coarse Mesh')
+#  e = gmres(A_coarse,-R_coarse.flatten(), np.zeros(np.shape(R_coarse.flatten())), tol=2e-04, restart=60, maxiter=60, xtype=None, M=None, callback=printnorm,restrt=None)[0]
+##  e = GMRes(A_coarse,-R_coarse.flatten(), np.zeros(np.shape(R_coarse.flatten())),30)
+#
+#  x0 = np.zeros(np.shape(main.a.a))
+#  x0[:] = np.reshape(sol,np.shape(main.a.a))
+#  x0[:,0:main.order/2,0:main.order/2,0:main.order/2] = x0[:,0:main.order/2,0:main.order/2,0:main.order/2] + np.reshape( e , np.shape(main_coarse.a.a) )
+#
+#  print(np.linalg.norm(sol),np.linalg.norm(x0)) 
+#
+#  print('Running Fine Mesh')
+#  sol = gmres(A, main.dt*R0.flatten(), x0=x0.flatten(), tol=3.5e-04, restart=60, maxiter=None, xtype=None, M=None, callback=printnorm,restrt=None)[0]
+##  sol = GMRes(A, main.dt*R0.flatten(), x0.flatten(),5)
+#
+#  R =  np.reshape( mv_resid(sol,main.dt*R0.flatten()) , np.shape(main.a.a))
+#  R_coarse = R[:,0:main.order/2,0:main.order/2,0:main.order/2]
+#  print('Running Coarse Mesh')
+#  e = gmres(A_coarse,-R_coarse.flatten(), np.zeros(np.shape(R_coarse.flatten())), tol=1e-05, restart=60, maxiter=60, xtype=None, M=None, callback=printnorm,restrt=None)[0]
+##  e = GMRes(A_coarse,-R_coarse.flatten(), np.zeros(np.shape(R_coarse.flatten())),20)
+#
+#  x0[:] = np.reshape(sol,np.shape(main.a.a))
+#  x0[:,0:main.order/2,0:main.order/2,0:main.order/2] = x0[:,0:main.order/2,0:main.order/2,0:main.order/2] + np.reshape( e , np.shape(main_coarse.a.a) )
+#  print(np.linalg.norm(sol),np.linalg.norm(e)) 
+#  sol = gmres(A, main.dt*R0.flatten(), x0=x0.flatten(), tol=1e-06, restart=60, maxiter=None, xtype=None, M=None, callback=printnorm,restrt=None)
+
+  main.comm.Barrier()
+  main.a.a = main.a0 + np.reshape(sol[0],np.shape(main.a.a))
+  main.a0[:] = np.reshape(sol[0],np.shape(main.a.a))
+  main.t += main.dt
+  main.iteration += 1
+
+
+
+def advanceSolImplicitPC(main,MZ,eqns,schemes):
+  coarsen = 4
+  main_coarse = variables(main.Nel,main.order/coarsen,main.quadpoints/(2*coarsen),eqns,main.mu,main.xG,main.yG,main.zG,main.t,main.et,main.dt,main.iteration,main.save_freq,schemes,'DNS',main.procx,main.procy)
+  old = np.zeros(np.shape(main.a0))
+  old[:] = main.a0[:]
+  main.a0[:] = main.a.a[:]
+  main_coarse.a.a[:] = main.a.a[:,0:main.order/coarsen,0:main.order/coarsen,0:main.order/coarsen]
+  main_coarse.a0[:] = main.a.a[:,0:main.order/coarsen,0:main.order/coarsen,0:main.order/coarsen]
+  main.getRHS(main,eqns,schemes)
+  main_coarse.getRHS(main_coarse,eqns,schemes)
+  R0 = np.zeros(np.shape(main.RHS))
+  R0[:] = main.RHS[:]
+  R0_coarse = np.zeros(np.shape(main_coarse.RHS))
+  R0_coarse[:] = main_coarse.RHS[:]
+  old_coarse = np.zeros(np.size(main_coarse.a.a))
   def mv(v):
     vr = np.reshape(v,np.shape(main.a.a))
     eps = 1.e-5
@@ -39,15 +140,47 @@ def advanceSolImplicitMG(main,MZ,eqns,schemes):
     return Av.flatten()
   def printnorm(r):
     print('GMRES norm = ' + str(np.linalg.norm(r)))
+  def printnormPC(r):
+    print('PC GMRES norm = ' + str(np.linalg.norm(r)))
+
+  def mv_coarse(v):
+    vr = np.reshape(v,np.shape(main_coarse.a.a))
+    eps = 1.e-5
+    main_coarse.a.a[:] = main_coarse.a0[:] + eps*vr
+    main_coarse.getRHS(main_coarse,eqns,schemes)
+    R1 = np.zeros(np.shape(main_coarse.RHS))
+    R1[:] = main_coarse.RHS[:]
+    Av = vr - main_coarse.dt/2.*(R1 - R0_coarse)/eps
+    main_coarse.a.a[:] = main_coarse.a0[:] 
+    return Av.flatten()
+
+  A_coarse = LinearOperator( (np.size(main_coarse.a.a),np.size(main_coarse.a.a)), matvec=mv_coarse )
+  def coarse_solve(v):
+    vf = np.reshape(v,np.shape(main.a.a))
+    vr = vf[:,0:main.order/coarsen,0:main.order/coarsen,0:main.order/coarsen]
+    print(np.linalg.norm(vr))
+#    sol2 = lgmres(A_coarse, vr.flatten(),x0=np.zeros(np.size(main_coarse.a.a)), tol=1e-02, maxiter=10, M=None, callback=printnormPC, inner_m=30, outer_k=3, outer_v=None, store_outer_Av=True)[0]
+    sol2 = lgmres(A_coarse, vr.flatten(),old_coarse, tol=1e-02, maxiter=10, M=None, callback=printnormPC, inner_m=30, outer_k=3, outer_v=None, store_outer_Av=True)[0]
+    old_coarse[:] = sol2[:]
+#   sol2 = bicgstab(A_coarse, vr.flatten(), x0=np.zeros(np.size(main_coarse.a.a)), tol=1e-02,maxiter=3, xtype=None, M=None, callback=printnormPC)[0]
+
+#    sol2 = GMRes(mv_coarse,vr.flatten(),x0=np.zeros(np.size(main_coarse.a.a)),nmax_iter = 10,restart=None)
+    solf = np.zeros(np.shape(main.a.a))
+    solf[:,0:main.order/coarsen,0:main.order/coarsen,0:main.order/coarsen] = np.reshape(sol2,np.shape(main_coarse.a.a) )
+    return solf.flatten()
+#    return v
 
   A = LinearOperator( (np.size(main.a.a),np.size(main.a.a)), matvec=mv )
-  sol = gmres(A, main.dt*R0.flatten(), x0=old.flatten(), tol=1e-07, restart=60, maxiter=None, xtype=None, M=None, callback=printnorm,restrt=None)
+  APC = LinearOperator( (np.size(main.a.a),np.size(main.a.a)), matvec=coarse_solve )
+
+  sol = gmres(A, main.dt*R0.flatten(), x0=old.flatten(), tol=1e-07, restart=60, maxiter=None, xtype=None, M=APC, callback=printnorm,restrt=None)
 #  sol = bicgstab(A, main.dt*R0.flatten(), x0=np.zeros(np.size(main.a.a)), tol=1e-07,maxiter=None, xtype=None, M=None, callback=printnorm)
   main.comm.Barrier()
   main.a.a = main.a0 + np.reshape(sol[0],np.shape(main.a.a))
   main.a0[:] = np.reshape(sol[0],np.shape(main.a.a))
   main.t += main.dt
   main.iteration += 1
+
 
 
 def advanceSolImplicit(main,MZ,eqns,schemes):
