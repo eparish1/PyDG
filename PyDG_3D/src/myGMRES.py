b28b519221f54f160e8b7a5663a6e35bc330ba30
Eric Parish <eric.parish9@gmail.com>
2017-05-09 07:41:57 -0400

    homemade gmres

--- PyDG_3D/src/myGMRES.py
+++ PyDG_3D/src/myGMRES.py
@@ -1,5 +1,105 @@
 import numpy as np
-def GMRes(Af, b, x0, nmax_iter, restart=None):
+def globalNorm(r,main):
+  ## Create Global residual
+  data = main.comm.gather(np.linalg.norm(r)**2,root = 0)
+  if (main.mpi_rank == 0):
+    rn_glob = 0.
+    for j in range(0,main.num_processes):
+      rn_glob += data[j]
+    rn_glob = np.sqrt(rn_glob)
+    for j in range(1,main.num_processes):
+      main.comm.send(rn_glob, dest=j)
+  else:
+    rn_glob = main.comm.recv(source=0)
+  return rn_glob
+
+def globalSum(r,main):
+  ## Create Global residual
+  data = main.comm.gather(np.sum(r),root = 0)
+  if (main.mpi_rank == 0):
+    rn_glob = 0.
+    for j in range(0,main.num_processes):
+      rn_glob += data[j]
+    for j in range(1,main.num_processes):
+      main.comm.send(rn_glob, dest=j)
+  else:
+    rn_glob = main.comm.recv(source=0)
+  return rn_glob
+
+
+
+def GMRes(Af, b, x0,main,tol=1e-9,maxiter_outer=1,maxiter=20, restart=None,printnorm=0):
+    k_outer = 0
+    bnorm = globalNorm(b,main)
+    error = 1.
+    while (k_outer < maxiter_outer and error >= tol):
+      r = b - Af(x0)
+      cs = np.zeros(maxiter) #should be the same on all procs
+      sn = np.zeros(maxiter) #same on all procs
+      e1 = np.zeros(np.size(b)) #should vary across procs
+      e1[0] = 1
+  
+      rnorm = globalNorm(r,main) #same across procs
+      Q = np.zeros((np.size(b),maxiter)) 
+      #v = [0] * (nmax_iter)
+      Q[:,0] = r / rnorm ## The first index of Q is across all procs
+      H = np.zeros((maxiter + 1, maxiter)) ### this should be the same on all procs
+      beta = rnorm*e1
+      k = 0
+      while (k < maxiter - 1  and error >= tol):
+  #    for k in range(0,nmax_iter-1):
+          Arnoldi(Af,H,Q,k,main)
+          apply_givens_rotation(H,cs,sn,k)
+          #update the residual vector
+          beta[k+1] = -sn[k]*beta[k]
+          beta[k] = cs[k]*beta[k]
+          error = abs(beta[k+1])/bnorm
+          if (main.mpi_rank == 0 and printnorm == 1):
+            print('Iteration = ' + str(k) + '  GMRES error = ' + str(error))
+          k += 1
+      y = np.linalg.solve(H[0:k,0:k],beta[0:k]) 
+      x = x0 + np.dot(Q[:,0:k],y)
+      x0[:] = x[:]
+      k_outer += 1
+    return x[:]
+
+
+def Arnoldi(Af,H,Q,k,main):
+    Q[:,k+1] = Af(Q[:,k])
+    for i in range(0,k+1):
+        H[i, k] = globalSum(Q[:,i]*Q[:,k+1],main)
+        Q[:,k+1] = Q[:,k+1] - H[i, k] * Q[:,i]
+    H[k + 1, k] = globalNorm(Q[:,k+1],main)
+#    if (h[k + 1, k] != 0 and k != nmax_iter - 1):
+    Q[:,k + 1] = Q[:,k+1] / H[k + 1, k]
+#    return h,v 
+
+def apply_givens_rotation(H, cs, sn, k):
+  #apply for ith column
+  for i in range(0,k):                              
+    temp     =  cs[i]*H[i,k] + sn[i]*H[i+1,k]
+    H[i+1,k] = -sn[i]*H[i,k] + cs[i]*H[i+1,k]
+    H[i,k]   = temp
+  
+  #update the next sin cos values for rotation
+  cs[k],sn[k] = givens_rotation( H[k,k], H[k+1,k])
+  
+  #eliminate H(i+1,i)
+  H[k,k] = cs[k]*H[k,k] + sn[k]*H[k+1,k]
+  H[k+1,k] = 0.0
+
+#----Calculate the Given rotation matrix----%%
+def givens_rotation(v1, v2):
+  if (v1==0):
+    cs = 0
+    sn = 1
+  else:
+    t=np.sqrt(v1**2+v2**2)
+    cs = np.abs(v1) / t
+    sn = cs * v2 / v1
+  return cs,sn
+
+def GMResOrig(Af, b, x0, nmax_iter, restart=None):
     r = b - Af(x0)
     rnorm = np.linalg.norm(r)
     v = [0] * (nmax_iter)
@@ -7,7 +107,7 @@ def GMRes(Af, b, x0, nmax_iter, restart=None):
     h = np.zeros((nmax_iter + 1, nmax_iter))
     for k in range(0,nmax_iter):
         w = Af(v[k])
-        for j in range(0,k):
+        for j in range(0,k+1):
             h[j, k] = np.dot(v[j], w)
             w = w - h[j, k] * v[j]
         h[k + 1, k] = np.linalg.norm(w)
@@ -18,6 +118,6 @@ def GMRes(Af, b, x0, nmax_iter, restart=None):
         result = np.linalg.lstsq(h, b2)[0]
         x = np.dot(np.asarray(v).transpose(), result) + x0
         r1 = b - Af(x)
-        print(np.linalg.norm(r1))
+        print(np.linalg.norm(r))
     return x[:]
 
