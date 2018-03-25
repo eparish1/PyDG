511acce939b53716b97763eb1897ff2cefacaf4d
Eric Parish <eric.parish9@gmail.com>
2018-03-25 14:55:22 -0400

    update setup

--- PyDG_3D/src_spacetime/setup.py
+++ PyDG_3D/src_spacetime/setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 from Cython.Compiler import Options
+import numpy
 Options.buffer_max_dims = 9
 extensions = [Extension(
                 "cython_euler",
                 sources=["cython_euler.pyx"],
                 extra_compile_args=["-fopenmp"],
-                extra_link_args=["-fopenmp"]
+                extra_link_args=["-fopenmp"],include_dirs=[numpy.get_include()]
             )]
 
 setup(
