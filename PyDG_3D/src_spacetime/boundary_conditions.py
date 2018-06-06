3ae43526c2e516d2b5fa6533e0dfd54de88a49e7
Eric Parish <eric.parish9@gmail.com>
2018-06-05 21:08:07 -0400

    updated isothermal wall

--- PyDG_3D/src_spacetime/boundary_conditions.py
+++ PyDG_3D/src_spacetime/boundary_conditions.py
@@ -275,8 +275,8 @@ def isothermalwall_bc(Ue,UBC,args,main,normals):
   p = (gamma - 1.)*(Ue[4] - 0.5*Ue[1]**2/Ue[0] - 0.5*Ue[2]**2/Ue[0] - 0.5*Ue[3]**2/Ue[0]) #extraploate pressure
   #print(p/(Ue[0]*R))
   T = Tw
-  #E = Cv*T  + 0.5*(uw**2 + vw**2 + ww**2) #wall velocity is zero
-  rhoE = p/(gamma - 1.) + 0.5*(uw**2 + vw**2 + ww**2)
+  E = Cv*T  + 0.5*(uw**2 + vw**2 + ww**2) #wall velocity is zero
+  #rhoE = p/(gamma - 1.) + 0.5*(uw**2 + vw**2 + ww**2)
   #p = rho R T
   UBC[0] = p/(R*T)
   UBC[1] = UBC[0]*uw
